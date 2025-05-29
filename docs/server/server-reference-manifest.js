3f1f165b58d1d98093d8f400837ef4996fada59b
Askold <makrikovas@gmail.com>
2025-05-29 13:59:39 +0300

    rebuild after merge

--- docs/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"z2sKVMStl+4g6zOfcBHWsycQYKXy2Jkeu08UggpDP+s=\"\n}"
--- docs/static/chunks/src_app_page_tsx_334befc9._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_4ad531c0._.js",
-  "static/chunks/src_components_hero_8bdb7363._.js",
-  "static/chunks/src_components_hero_HeroSection_module_scss_module_0d080112.css"
-],
-    source: "dynamic"
-});
